c29ca4b1c286e5324966a35fd49b9f96de3a6c9d
Pablo RIbeiro <pableeenho@gmail.com>
2019-09-14 21:33:07 -0300

    Pagina inicial do Swagger

--- api.py
+++ api.py
@@ -1,10 +1,24 @@
 #!flask/bin/python
 from flask import Flask
+from flask_restplus import Resource, Api
 from flask import request
 from service import Recomendacao
+from flask_swagger_ui import get_swaggerui_blueprint
 import simplejson as json
 
 app = Flask(__name__)
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+    }
+)
+
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 @app.route('/recomendacoes/capturas', methods=['PUT'])
 def capturaDados():
